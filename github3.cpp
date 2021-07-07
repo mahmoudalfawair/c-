96cb4a630ee203597f71cc5ad3a24c1979cf8972
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-07 08:58:14 +0300

    Add files via upload

---
+++ github3.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+int x =1 ;
+int q =2 ;
+int w =3 ;
+int e =4 ; 
+cout << "x is "<< x <<endl;
+cout << "q is "<< q <<endl;
+cout << "w is "<< w <<endl;
+cout << "e is "<< e <<endl;
+
+return 0;
+}
