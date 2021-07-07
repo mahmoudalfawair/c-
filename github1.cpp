0faf9869bd6be60922221b0f50fadeee3a57da8f
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-07 08:34:19 +0300

    Add files via upload

---
+++ github1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+int var1 ;
+cin >> var1;
+
+int var2 ; 
+cin >> var2;
+ 
+cout << var1 << "+" << var2 << "="<< var1 + var2 << endl;
+return 0;
+}
